12d0ba60741f802add12b4cb96881cd911e7329b
VrishaanganVS <teknobot24@gmail.com>
2023-11-06 19:18:58 +0530

    Update employee.py

--- employee.py
+++ employee.py
@@ -9,6 +9,22 @@ class employeeClass:
         self.root.config(bg='white')
         self.root.focus_force()
 
+        #==============
+        #All variables========
+        self.var_searchby=StringVar()
+        self.var_searchtxt=StringVar()
+        
+        self.var_pass=StringVar()
+        self.var_emp_id=StringVar()
+        self.var_gender=StringVar()
+        self.var_contact=StringVar()
+        self.var_email=StringVar()
+        self.var_dob=StringVar()
+        self.var_doj=StringVar()
+        self.var_pass=StringVar()
+        self.var_utype=StringVar()
+        
+        
 
         #===searhFrame===#
         SearchFrame=LabelFrame(self.root,text="Search Employee",font=('goudy old style',12,'bold'),bg='white')
@@ -16,16 +32,26 @@ class employeeClass:
   
 
          #===options===#
-        cmd_search=ttk.Combobox(SearchFrame,values=("Select","Email","Name","Contact"),state='readonly',justify=CENTER,font=("goudy old style",15))
+        cmd_search=ttk.Combobox(SearchFrame,textvariable=self.var_searchby,values=("Select","Email","Name","Contact"),state='readonly',justify=CENTER,font=("goudy old style",15))
         cmd_search.place(x=10,y=10,width=180)
         cmd_search.current(0)
 
-        txt_search = Entry(SearchFrame, font=('goudy old style', 15), bg='lightyellow').place(x=200,y=10)
-        #txt_search.place(x=200, y=10, width=180)
+        txt_search = Entry(SearchFrame,textvariable=self.var_searchtxt,font=('goudy old style', 15), bg='lightyellow').place(x=200,y=10)
+        btn_search= Button(SearchFrame,text="Search",font=('goudy old style', 15), bg='#4caf50',fg="white",cursor="hand2").place(x=410,y=9,width=150,height=30)
 
 
+        #=====title======#
+        title=Label(self.root,text="Employee Details",font=("goudy old style",15),bg="#0f4d7d",fg="white").place(x=50,y=100,width=1000)
 
 
+            
+        #=====content====#
+        
+        lbl_empid=Label(self.root,text="Emp ID",font=("goudy old style",15),bg="white").place(x=50,y=150)                    
+            
+            
+            
+            
 
 if __name__=='__main__':
     root=Tk()
