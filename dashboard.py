d78b689394498e3d6a99c833bf87bd67c79175b9
Sujal Bhandari <116736283+Suj4LB@users.noreply.github.com>
2023-11-06 16:51:48 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -11,7 +11,7 @@ class IMS:
         title=Label(self.root,text="Shopping Managment System",image=self.icon_title,compound=LEFT,font=('times new roman',40,"bold"),bg="#010c48",fg='white').place(x=0,y=0,relwidth=1,height=70)
 
         #===btnLogout===#
-        btn_logout=Button(self.root,text="Logout",font=("times new roman",15,'bold'),bg="yellow",cursor='hand2').place(x=1100,y=10,height=30,width=150)
+        btn_logout=Button(self.root,text="Logout",font=("times new roman",15,'bold'),bg="yellow",cursor='hand2').place(x=1350,y=10,height=30,width=150)
         #===clock===#
         self.lbl_clock=Label(self.root,text="Welcome to Shopping Managment System\t\t Date: DD-MM-YYYY\t\t Time: HH:MM:SS",font=('times new roman',15),bg="#4d636d",fg='white')
         self.lbl_clock.place(x=0,y=70,relwidth=1,height=30)
