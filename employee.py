b5212f67c01210237742a95f46afe762dea197b4
Arjun02BH <121733134+Arjun02BH@users.noreply.github.com>
2023-11-06 18:15:07 +0530

    Configured the geometry for employee microservice

--- employee.py
+++ employee.py
@@ -4,6 +4,10 @@ from tkinter import ttk
 class employeeClass:
     def __init__ (self,root):
         self.root=root
+        self.root.geometry("1073x523+200+123")
+        self.root.title("Shopping Managment System  | Developed by Team 3")
+        self.root.config(bg='white')
+        self.root.focus_force()
 
 if __name__=='__main__':
     root=Tk()
