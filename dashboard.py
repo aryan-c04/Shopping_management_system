904b4d7cf8c47a2d9ef30a5d4d2f25f22b04b702
Sujal Bhandari <116736283+Suj4LB@users.noreply.github.com>
2023-11-06 16:36:56 +0530

    Create dashboard.py

---
+++ dashboard.py
@@ -0,0 +1,14 @@
+from tkinter import*
+from PIL import Image, ImageTk
+class IMS:
+    def __init__ (self,root):
+        self.root=root
+        self.root.geometry=("1350*700+0+0")
+        self.root.title("Shopping Managment System  | Developed by Team 3")
+        self.root.config(bg='white')
+        #===Title===#
+        self.icon_title=PhotoImage(file="images/logo1.png")
+        title=Label(self.root,text="Shopping Managment System",image=self.icon_title,compound=LEFT,font=('times new roman',40,"bold"),bg="#010c48",fg='white').place(x=0,y=0,relwidth=1,height=70)
+
+        #===btnLogout===#
+        btn_logout=Button(self.root,text="Logout",font=("times new roman",15,'bold'),bg="yellow",cursor='hand2').place(x=1100,y=10,height=30,width=150)
