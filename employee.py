7bd6ee6790648ca9f441458104e91bcfb5d901d2
Arjun02BH <121733134+Arjun02BH@users.noreply.github.com>
2023-12-03 23:50:51 +0530

    Create row1 in employee.py

--- employee.py
+++ employee.py
@@ -49,6 +49,18 @@ class employeeClass:
         
         lbl_empid=Label(self.root,text="Emp ID",font=("goudy old style",15),bg="white").place(x=50,y=150)                    
             
+
+        #===row1===#
+        lbl_empid=Label(self.root,text="Emp ID",font=("goudy old style",15),bg="white").place(x=50,y=150)       
+        lbl_gender=Label(self.root,text="Gender",font=("goudy old style",15),bg="white").place(x=350,y=150)                    
+        lbl_contact=Label(self.root,text="Contact",font=("goudy old style",15),bg="white").place(x=750,y=150)                                 
+        
+
+        txt_empid=Entry(self.root,textvariable=self.var_emp_id,font=("goudy old style",15),bg="lightyellow").place(x=150,y=150,width=180)       
+        cmd_gender=ttk.Combobox(self.root,textvariable=self.var_gender,values=("Select","Male","Female","Other"),state='readonly',justify=CENTER,font=("goudy old style",15))
+        cmd_gender.place(x=500,y=150,width=180) 
+        cmd_gender.current(0)
+        txt_contact=Entry(self.root,textvariable=self.var_contact,font=("goudy  old style",15),bg="lightyellow").place(x=850,y=150,width=180)           
             
             
             
